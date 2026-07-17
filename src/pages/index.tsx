1b2dd3f3181a832189ca37d874fcc55751c96e02
Tommy Oh <koa18@sfu.ca>
2026-07-16 21:38:00 -0700

    add pre-commit hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import type {ReactNode} from 'react';
-import {Redirect} from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import type { ReactNode } from "react";
+import { Redirect } from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Home(): ReactNode {
-  const destination = useBaseUrl('/docs/version2/project-design');
+  const destination = useBaseUrl("/docs/version2/project-design");
 
   return <Redirect to={destination} />;
 }
